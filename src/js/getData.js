a371430ca52c0dc35d820b959491556af8041879
Thomas SELLE <thomas.servuc@gmail.com>
2016-11-19 11:08:10 +0100

    Improve data

--- src/js/getData.js
+++ src/js/getData.js
@@ -14,7 +14,12 @@ function GetData(boatId) {
 		country: "",
 		lat: 0.0,
 		lon: 0.0,
-		speed: 0.0
+		speed: 0.0,
+		deadWeight: 0.0,
+		grossTonnage: 0.0,
+		size: "",
+		originalPort: "",
+		speedRecordAverage: ""
 	};
 
 	return this;
@@ -51,6 +56,11 @@ GetData.prototype = {
 		this.extractDataTitle(myDiv);
 		this.extractDataType(myDiv);
 		this.extractDataCountry(myDiv);
+		this.extractDataGrossTonnage(myDiv);
+		this.extractDataGrossDeadWeight(myDiv);
+		this.extractDataSize(myDiv);
+		this.extractDataOriginalPort(myDiv);
+		this.extractDataSpeedRecordAverage(myDiv);
 	},
 
 	extractDataImg: function (jqueryNode) {
@@ -67,5 +77,27 @@ GetData.prototype = {
 
 	extractDataCountry: function (jqueryNode) {
 		this.data.country = jqueryNode.find(".bg-info > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > b:nth-child(2)").text();
+	},
+
+	extractDataGrossTonnage: function (jqueryNode) {
+		this.data.grossTonnage = parseFloat(jqueryNode.find(".bg-info > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > b:nth-child(2)").text());
+	},
+
+	extractDataGrossDeadWeight: function (jqueryNode) {
+		this.data.deadWeight = parseFloat(jqueryNode.find(".bg-info > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > b:nth-child(2)").text());
+	},
+
+	extractDataSize: function (jqueryNode) {
+		this.data.size = jqueryNode.find(".bg-info > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > b:nth-child(2)").text();
+	},
+
+	extractDataOriginalPort: function (jqueryNode) {
+		this.data.originalPort = jqueryNode.find("li.group-ib:nth-child(9) > span:nth-child(2) > b:nth-child(1)").text();
+	},
+
+	extractDataSpeedRecordAverage: function (jqueryNode) {
+		this.data.speedRecordAverage = jqueryNode.find("div.table-responsive:nth-child(6) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > b:nth-child(1)").text();
 	}
 };
+
+
