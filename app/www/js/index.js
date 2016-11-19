e5e62ac7c2e7e9127244baf363177486c04a5abf
Matthieu Vion <mvion@trace-software.com>
2016-11-19 03:43:15 +0100

    Adding background camera view
    Force landscape orientation
    Force app in fullscreen

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -32,15 +32,20 @@ var app = {
 
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
-    }
+        this.initializeCamera();
+    },
+
+	initializeCamera: function () {
+		CameraPreview.startCamera(
+			{
+				camera: "rear",
+				tapPhoto: false,
+				toBack: true
+			}
+		);
+
+	}
 };
 
 app.initialize();
